de7639b5798107d8847952473f1134bc0e4f5a41
Patrick <contact@patrick-segarel.com>
2025-11-23 11:28:11 +0700

    Updates company information

--- src/lib/config/company.ts
+++ src/lib/config/company.ts
@@ -1,13 +1,13 @@
 export const companyConfig = {
-	name: 'YOUR COMPANY NAME',
+	name: 'INSENSE COMPANY LIMITED',
 	addressLine1: '7th Floor',
 	addressLine2: '60 Nguyen Van Thu',
 	ward: 'Tan Dinh Ward',
 	city: 'Ho Chi Minh City',
-	taxCode: '0101234567',
-	representative: 'Mr. Director',
-	function: 'Director',
-	representativePhone: '+84 123 456 789',
-	representativeEmail: 'director@company.com',
+	taxCode: '0108958239',
+	representative: 'Mr. Patrick Segarel',
+	function: 'CEO',
+	representativePhone: '+84 (0) 37 7920 759',
+	representativeEmail: 'patrick@insense.vn',
 	taxRates: [10, 20]
 };
